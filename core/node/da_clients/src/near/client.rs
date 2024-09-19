ecd90620b9cac7af2db5c5ed19116716658b294f
vikinatora <vikinatora@gmail.com>
2024-09-19 10:23:20 +0300

    fmt

--- core/node/da_clients/src/near/client.rs
+++ core/node/da_clients/src/near/client.rs
@@ -1,13 +1,8 @@
-use anyhow::anyhow;
-use async_trait::async_trait;
 use std::{fmt::Debug, sync::Arc};
 
-use near_primitives::borsh;
-
-use zksync_da_client::{
-    types::{self, DAError},
-    DataAvailabilityClient,
-};
+use async_trait::async_trait;
+use zksync_config::NearConfig;
+use zksync_da_client::{types, DataAvailabilityClient};
 
 use crate::{
     near::{
@@ -16,8 +11,6 @@ use crate::{
     },
     utils::to_non_retriable_da_error,
 };
-
-use zksync_config::NearConfig;
 
 #[derive(Clone)]
 pub struct NearClient {
